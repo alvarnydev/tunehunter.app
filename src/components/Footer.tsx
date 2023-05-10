1efd625ec6dfd13b1eb952194c899c2d71cf2143
alvarnydev <me@alvarny.dev>
2023-05-10 10:07:23 +0200

    Language selector

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,57 @@
 import { BsGithub } from 'react-icons/bs';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { themeChange } from 'theme-change';
+import { DE, ES, GB, US } from 'country-flag-icons/react/3x2';
+
+const ThemeSelector = () => (
+  <select data-choose-theme className='select'>
+    <option disabled selected>
+      Pick a theme
+    </option>
+    <option value='light'>Light</option>
+    <option value='dark'>Dark</option>
+    <option value='synthwave'>Synthwave</option>
+    <option value='valentine'>Valentine</option>
+  </select>
+);
+
+const LanguageSelector = () => {
+  const [language, setLanguage] = useState('English');
+
+  function handleClick(e: React.MouseEvent<HTMLAnchorElement>) {
+    const lang = e.currentTarget.firstChild?.textContent;
+    if (lang == null) return;
+
+    setLanguage(lang);
+  }
+
+  return (
+    <div className='dropdown dropdown-top'>
+      <label tabIndex={0} className='btn m-1 border-0 bg-base-100 hover:bg-current'>
+        {language === 'English' && <GB title='English' className='w-8 rounded' />}
+        {language === 'German' && <DE title='German' className='w-8 rounded' />}
+        {language === 'Spanish' && <ES title='Spanish' className='w-8 rounded' />}
+      </label>
+      <ul tabIndex={0} className='dropdown-content menu p-2 shadow bg-base-100 rounded-box w-36'>
+        <li>
+          <a onClick={handleClick}>
+            <GB title='English' className='w-8 rounded' /> English
+          </a>
+        </li>
+        <li>
+          <a onClick={handleClick}>
+            <DE title='German' className='w-8 rounded' /> German
+          </a>
+        </li>
+        <li>
+          <a onClick={handleClick}>
+            <ES title='Spanish' className='w-8 rounded' /> Spanish
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 const Footer = () => {
   useEffect(() => {
@@ -8,52 +59,12 @@ const Footer = () => {
   }, []);
 
   return (
-    <footer className='absolute bottom-0 w-full flex justify-around py-5'>
-      <select className='select'>
-        <option disabled selected>
-          Region
-        </option>
-        <option value='eu'>EU</option>
-        <option value='us'>US</option>
-      </select>
-      <a href='https://github.com/alvarnydev/btt.com' target='_blank' className='btn btn-ghost'>
+    <footer className='absolute bottom-0 w-full flex justify-around py-5 bg-base-300'>
+      <LanguageSelector />
+      <a href='https://github.com/alvarnydev/btt.com' target='_blank' className='btn'>
         <BsGithub size={24} />
       </a>
-      <select data-choose-theme className='select'>
-        <option disabled selected>
-          Pick a theme
-        </option>
-        <option value='light'>Light</option>
-        <option value='dark'>Dark</option>
-        <option value='synthwave'>Synthwave</option>
-        <option value='valentine'>Valentine</option>
-        {/* 
-        <option value='cupcake'>Cupcake</option>
-        <option value='bumblebee'>Bumblebee</option>
-        <option value='emerald'>Emerald</option>
-        <option value='corporate'>Corporate</option>
-        <option value='retro'>Retro</option>
-        <option value='cyberpunk'>Cyberpunk</option>
-        <option value='halloween'>Halloween</option>
-        <option value='garden'>Garden</option>
-        <option value='forest'>Forest</option>
-        <option value='aqua'>Aqua</option>
-        <option value='lofi'>Lofi</option>
-        <option value='pastel'>Pastel</option>
-        <option value='fantasy'>Fantasy</option>
-        <option value='wireframe'>Wireframe</option>
-        <option value='black'>Black</option>
-        <option value='luxury'>Luxury</option>
-        <option value='dracula'>Dracula</option>
-        <option value='cmyk'>Cmyk</option>
-        <option value='autumn'>Autumn</option>
-        <option value='business'>Business</option>
-        <option value='acid'>Acid</option>
-        <option value='lemonade'>Lemonade</option>
-        <option value='night'>Night</option>
-        <option value='coffee'>Coffee</option>
-        <option value='winter'>Winter</option> */}
-      </select>
+      <ThemeSelector />
     </footer>
   );
 };
