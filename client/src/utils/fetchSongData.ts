a07bc7e9f76f74f10b12d7f4608553b3e703229d
Yannik Sahl <yanniksahl@outlook.de>
2023-09-12 20:54:50 +0200

    ts config

--- client/src/utils/fetchSongData.ts
+++ client/src/utils/fetchSongData.ts
@@ -1,7 +1,5 @@
 export const fetchSongData = async () => {
-  const dataUrl = new URL(
-    'https://itunes.apple.com/search?term=need+to+feel+loved&country=DE&media=music&entity=song&limit=5'
-  ).href;
-  const response = await fetch(dataUrl).then((res) => res.json());
-  return response;
+  const dataUrl = new URL('https://api.buythattrack.com/itunes?song=test&artist=test&country=DE')
+    .href;
+  return await fetch(dataUrl).then((res) => res.json());
 };
--- server/index.js
+++ server/index.js
@@ -1,29 +1,17 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
+const express_1 = require("express");
+const dotenv_1 = require("dotenv");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
-app.get('/itunes', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get('/itunes', async (req, res) => {
     let { song, artist, country } = req.query;
     if (!country) {
         country = 'DE';
     }
     const dataUrl = new URL(`https://itunes.apple.com/search?term=${song}+${artist}&country=${country}&media=music&entity=song&limit=5`).href;
-    const response = yield fetch(dataUrl).then((res) => res.json());
+    const response = await fetch(dataUrl).then((res) => res.json());
     const filteredResponse = response.results.map((song) => {
         return {
             kind: song.kind,
@@ -36,7 +24,7 @@ app.get('/itunes', (req, res) => __awaiter(void 0, void 0, void 0, function* () 
         };
     });
     res.send({ response, filteredResponse });
-}));
+});
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
