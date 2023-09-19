eaae72713889c5fa39415a523529e741ceb12003
Yannik Sahl <yanniksahl@outlook.de>
2023-09-19 18:11:27 +0200

    mvp itunes

--- client/src/components/utils/Loading.tsx
+++ client/src/components/utils/Loading.tsx
@@ -2,13 +2,16 @@ import { ScaleLoader } from 'react-spinners';
 
 export const LoadingSpinner = (props: { size: number }) => {
   return (
-    <ScaleLoader
-      height={props.size}
-      width={props.size / 5}
-      margin={props.size / 10}
-      radius={props.size}
-      color='#36d7b7'
-    />
+    <div className='h-[426px] flex'>
+      <ScaleLoader
+        height={props.size}
+        width={props.size / 5}
+        margin={props.size / 10}
+        radius={props.size}
+        color='#36d7b7'
+        className='text-primary m-auto'
+      />
+    </div>
   );
 };
 
