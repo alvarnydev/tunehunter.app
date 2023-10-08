65a8b105ba388739327367dabbe9f5cba5a7c28f
Yannik Sahl <yanniksahl@outlook.de>
2023-10-08 12:08:16 +0200

    theme changes

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import { themeChange } from 'theme-change';
 import ThemeSelector from './Footer/ThemeSelector';
 import LanguageSelector from './Footer/LanguageSelector';
-import GithubLinker from './Footer/GithubLinker';
 import ProfileButton from './Footer/ProfileButton';
 
 const Footer = () => {
@@ -13,7 +12,6 @@ const Footer = () => {
   return (
     <footer className='w-full grid grid-cols-3 md:py-3 py-1 bg-base-300'>
       <ThemeSelector />
-      {/* <GithubLinker /> */}
       <ProfileButton />
       <LanguageSelector />
     </footer>
--- client/src/components/Footer/ProfileButton.tsx
+++ client/src/components/Footer/ProfileButton.tsx
@@ -2,14 +2,9 @@ import { RiAccountCircleFill } from 'react-icons/ri';
 
 const ProfileButton = () => (
   <div className='flex justify-center items-center'>
-    <a
-      href='https://github.com/alvarnydev/buythattrack.com'
-      target='_blank'
-      className='btn'
-      aria-label='Link to the GitHub page of the project'
-    >
-      <RiAccountCircleFill size={36} className='' />
-    </a>
+    <div className='btn btn-primary rounded-full'>
+      <RiAccountCircleFill size={40} className='text-white' />
+    </div>
   </div>
 );
 
