1f47caab2112d6f5ab14e9c616a2308da1a86800
Yannik Sahl <me@yanniksahl.com>
2023-11-27 22:02:45 +0100

    layout changes

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,11 +1,14 @@
 import { PropsWithChildren } from 'react';
 import Footer from './Footer';
+import { useAuth } from '../contexts/auth';
 
 const Layout = ({ children }: PropsWithChildren) => {
+  const { isAuthenticated } = useAuth();
+
   return (
     <div className='min-h-screen flex flex-col justify-between items-center overflow-hidden rotate-0'>
       {/* <Header /> */}
-      <main className='flex-1 flex flex-col justify-center items-center w-full max-w-5xl py-16 gap-16 md:gap-[5vh]'>{children}</main>
+      <main className={`flex-1 flex flex-col justify-center items-center w-full max-w-5xl py-16 gap-16 ${isAuthenticated ? 'md:gap-[4.6vh]' : 'md:gap-[2.8vh]'}`}>{children}</main>
       <Footer />
     </div>
   );
