0cdc77d5efe5a8c6a349679e1a0ed0e87e8bfae9
Yannik Sahl <me@yanniksahl.com>
2023-12-02 20:13:01 +0100

    buythattrack.com > traxtrove.com

--- client/src/components/utils/ErrorFunctions.ts
+++ client/src/components/utils/ErrorFunctions.ts
@@ -1,11 +1,8 @@
 import { sendMessage } from './TelegramUtils';
 
-export const logError = (
-  error: Error,
-  info: { customMessage?: string; componentStack?: string }
-) => {
+export const logError = (error: Error, info: { customMessage?: string; componentStack?: string }) => {
   sendMessage(
-    `buythattrack.com error! %0A
+    `traxtrove.com error! %0A
     Error message: ${error.message} %0A
     Custom error message: ${info.customMessage} %0A
     Stack trace: ${info.componentStack}`
