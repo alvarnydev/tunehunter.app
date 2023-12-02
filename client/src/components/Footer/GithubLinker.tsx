b5d9bcf0e7f7b439354216bdcb05d7e57f94e415
Yannik Sahl <me@yanniksahl.com>
2023-12-02 20:15:09 +0100

    name change #2

--- client/src/components/Footer/GithubLinker.tsx
+++ client/src/components/Footer/GithubLinker.tsx
@@ -2,12 +2,7 @@ import { BsGithub } from 'react-icons/bs';
 
 const GithubLinker = () => (
   <div className='flex justify-center items-center'>
-    <a
-      href='https://github.com/alvarnydev/buythattrack.com'
-      target='_blank'
-      className='btn'
-      aria-label='Link to the GitHub page of the project'
-    >
+    <a href='https://github.com/alvarnydev/traxtrove.com' target='_blank' className='btn' aria-label='Link to the GitHub page of the project'>
       <BsGithub size={28} />
     </a>
   </div>
