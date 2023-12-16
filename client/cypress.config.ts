f782127362af97130c7dc01cd0fc43165366baff
Yannik Sahl <me@yanniksahl.com>
2023-12-16 09:27:14 +0100

    cypress

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from "cypress";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents(_on, _config) {
+      // _ by me
       // implement node event listeners here
     },
   },
