1eebcec43ded19d2b557a72f0572b7a0d1278460
Yannik Sahl <me@yanniksahl.com>
2023-12-20 11:29:20 +0100

    Jest & Vercel Speed Insights

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -2,6 +2,7 @@ import { PropsWithChildren } from 'react';
 import Footer from './Footer';
 import Particles from './utils/Particles';
 import { useTheme } from '../contexts/theme';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const Layout = ({ children }: PropsWithChildren) => {
   const { theme } = useTheme();
@@ -10,7 +11,10 @@ const Layout = ({ children }: PropsWithChildren) => {
     <div className='min-h-screen flex flex-col justify-between items-center overflow-hidden rotate-0'>
       {/* <Header here /> */}
       {theme === 'synthwave' && <Particles className='absolute inset-0 -z-10 animate-fade-in' quantity={100} />}
-      <main className='relative flex-1 flex flex-col justify-center items-center w-full max-w-5xl py-16 gap-10'>{children}</main>
+      <main className='relative flex-1 flex flex-col justify-center items-center w-full max-w-5xl py-16 gap-10'>
+        {children}
+        <SpeedInsights />
+      </main>
       <Footer />
     </div>
   );
