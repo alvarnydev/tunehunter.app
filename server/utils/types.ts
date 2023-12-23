c428df9e7333548d719c60f77a2215c3f0773bcd
Yannik Sahl <me@yanniksahl.com>
2023-12-23 16:24:42 +0100

    Moved server types out of globalTypes

--- server/utils/types.ts
+++ server/utils/types.ts
@@ -15,3 +15,45 @@ interface RequestQuery {
 
 export type OurRequest = Request<RequestParams, ResponseBody, RequestBody, RequestQuery>;
 export type OurResponse = Response<ResponseBody>;
+
+// What the itunes API returns
+export type ITunesDataType = {
+  resultCount: number;
+  results: ITunesResultsType[];
+};
+
+export type ITunesResultsType = {
+  wrapperType: string;
+  kind: string;
+  artistId: number;
+  collectionId: number;
+  trackId: number;
+  artistName: string;
+  collectionName: string;
+  trackName: string;
+  collectionCensoredName: string;
+  trackCensoredName: string;
+  artistViewUrl: string;
+  collectionViewUrl: string;
+  trackViewUrl: string;
+  previewUrl: string;
+  artworkUrl30: string;
+  artworkUrl60: string;
+  artworkUrl100: string;
+  collectionPrice?: number;
+  trackPrice?: number;
+  releaseDate: string;
+  collectionExplicitness: string;
+  trackExplicitness: string;
+  discCount: number;
+  discNumber: number;
+  trackCount: number;
+  trackNumber: number;
+  trackTimeMillis: number;
+  country: string;
+  currency: string;
+  primaryGenreName: string;
+  contentAdvisoryRating?: string;
+  isStreamable: boolean;
+  collectionArtistName?: string;
+};
