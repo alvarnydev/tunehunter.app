f235e4d1d42f284dc502ec78fbfb490cfb4b83b7
Yannik Sahl <me@yanniksahl.com>
2024-01-04 13:51:53 +0200

    Header tag-line (for now invisible)

--- client/src/components/atoms/Header.tsx
+++ client/src/components/atoms/Header.tsx
@@ -9,11 +9,16 @@ const Header = () => {
   };
 
   return (
-    <button className='flex items-center mb-4 shadow-primary drop-shadow-xl' onClick={handleClick}>
-      <FaCompactDisc className='text-2xl mr-2 text-base-content ' />
-      <h1 className='text-5xl text-primary'>Rekord</h1>
-      <h1 className='text-5xl text-secondary'>Store</h1>
-    </button>
+    <div className='mb-4'>
+      <button className='flex items-center shadow-primary drop-shadow-xl' onClick={handleClick}>
+        <FaCompactDisc className='text-2xl mr-2 text-base-content ' />
+        <h1 className='text-5xl text-primary'>Rekord</h1>
+        <h1 className='text-5xl text-secondary'>Store</h1>
+      </button>
+      {/* <h3 className='text-right mr-2'>
+        <i>Rekord shopping in one click</i>
+      </h3> */}
+    </div>
   );
 };
 
