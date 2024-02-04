344b0fb9e1422d50231f2366c63f772cb0466967
Yannik Sahl <me@yanniksahl.com>
2024-02-04 10:09:09 +0100

    wip banner

--- client/src/components/pages/Layout.tsx
+++ client/src/components/pages/Layout.tsx
@@ -4,6 +4,7 @@ import Footer from '@/components/organisms/Footer';
 import { useTheme } from '@/contexts/theme';
 import Particles from '@/components/atoms/Particles';
 import Header from '../atoms/Header';
+import InfoBanner from '../atoms/InfoBanner';
 
 const Layout = ({ children }: PropsWithChildren) => {
   const { theme } = useTheme();
@@ -11,6 +12,7 @@ const Layout = ({ children }: PropsWithChildren) => {
   return (
     <div className='min-h-screen flex flex-col justify-between items-center overflow-hidden rotate-0'>
       {theme === 'synthwave' && <Particles className='absolute inset-0 -z-10 animate-fade-in' quantity={100} />}
+      <InfoBanner className='w-full' message='Work in Progress ' />
       <main className='relative flex-1 flex flex-col justify-center items-center w-full max-w-5xl py-16 gap-10'>
         <Header />
         {children}
